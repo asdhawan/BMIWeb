7229401403a38911c7b985a71a779ddb11269075
asdhawan <angad.dhawan@gmail.com>
2018-02-01 15:46:28 -0800

    .

---
+++ BMIWebUI/BMIWebUI/Models/Document.cs
@@ -0,0 +1,8 @@
+﻿namespace BMIWebUI.Models {
+    public class Document {
+        public int DocumentId { get; set; }
+        public string DocumentName { get; set; }
+        public bool DocuSignableYN { get; set; }
+        public string DocuSignTemplateId { get; set; }
+    }
+}
